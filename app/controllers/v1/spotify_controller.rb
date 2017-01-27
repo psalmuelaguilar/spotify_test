ef89dd78036cea9a95192bb1ebefebe652c1269b
psalmuelaguilar <psalmuelaguilar@gmail.com>
2017-01-27 11:16:22 +0800

    Move to api only

--- app/controllers/v1/spotify_controller.rb
+++ app/controllers/v1/spotify_controller.rb
@@ -1,12 +1,10 @@
-class SpotifyController < ApplicationController
+class V1::SpotifyController < ApplicationController
   def index
     if params[:spotify_search].present?
       # FETCH SERVICE
       service = SpotifyServices::Search.new({query: params[:spotify_search]})
       response = service.fetch
-      if response.code == 200
-        @response = service.process
-      end
+      render json: service.process
     end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@ SpotifyTest::Application.routes.draw do
   root "spotify#index"
 
   resources :spotify
+  namespace :v1, constraints: ApiConstraints.new(version: 1, default: true) do
+    resources :spotify
+  end
 end
